e1f5c7fd4216f074d9dae9eccf0d70948c5c1e1e
linzyspace <linzyspace@gmail.com>
2025-11-21 07:08:00 -0800

    Update cloudrun-deploy.sh

--- infra/cloudrun-deploy.sh
+++ infra/cloudrun-deploy.sh
@@ -1,1 +1,14 @@
+#!/usr/bin/env bash
+PROJECT=${PROJECT:-your-gcp-project}
+REGION=${REGION:-us-central1}
+SERVICE=blog-assistant-api
 
+
+gcloud builds submit --tag gcr.io/$PROJECT/$SERVICE:latest api
+
+
+gcloud run deploy $SERVICE \
+--image gcr.io/$PROJECT/$SERVICE:latest \
+--region $REGION \
+--platform managed \
+--allow-unauthenticated
