2e3ee8fedd01714b6a072c202679ec59b80c5242
linzyspace <linzyspace@gmail.com>
2025-11-22 10:06:12 -0800

    Update bloggerController.js

--- backend/controllers/bloggerController.js
+++ backend/controllers/bloggerController.js
@@ -1,1 +1,25 @@
+const bloggerService = require("../services/bloggerService");
+const { searchPosts } = require("../utils/searchUtils");
+
+exports.handleChat = async (req, res) => {
+  try {
+    const { message } = req.body;
+    if (!message) return res.json({ reply: "Please send a message." });
+
+    // Fetch all posts
+    const posts = await bloggerService.getAllPosts();
+
+    // Search posts
+    const result = searchPosts(posts, message);
+
+    if (!result) {
+      return res.json({ reply: "Sorry, I couldn't find anything relevant in your blog." });
+    }
+
+    return res.json({ reply: result });
+  } catch (err) {
+    console.error(err);
+    res.json({ reply: "Error fetching blog content." });
+  }
+};
 
